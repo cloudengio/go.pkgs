a2db53505dd6c061e35a8c25e60b9c64a39a13f2
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2020-03-31 13:17:17 -0700

    errors: rename FileLocation to the more idiomatic Caller. (#9)
    As a consequence rename the existing Caller etc as WithCaller.

--- errors/doc.go
+++ errors/doc.go
@@ -3,19 +3,23 @@
 // license that can be found in the LICENSE file.
 
 // Package errors provides utility routines for working with errors that
-// are compatible with go 1.13+ and for annotating errors with location
-// and other information. It provides errors.M which can be used
-// to collect and work with multiple errors in a thread safe manner.
-// It also provides convenience routines for annotating existing errors
-// with caller and other information.
+// are compatible with go 1.13+ and for annotating errors. It provides
+// errors.M which can be used to collect and work with multiple errors in a
+// thread safe manner. It also provides convenience routines for annotating
+// existing errors with caller and other information.
 //
 //   errs := errors.M{}
 //   errs.Append(fn(a))
 //   errs.Append(fn(b))
 //   err := errs.Err()
 //
+//
+// The location of a function's immediate caller (depth of 1) in form of the
+// directory/filename:<line> (name len of 2) can be obtained as follows:
+//   errors.Caller(1, 2)
+//
 // Annotations, can be added as follows:
-//   err := errors.Caller(os.ErrNotExist)
+//   err := errors.WithCaller(os.ErrNotExist)
 //
 // Where:
 //   fmt.Printf("%v\n", err)
@@ -27,8 +31,8 @@
 //
 // Annotated errors can be passed to errors.M:
 //   errs := errors.M{}
-//   errs.Append(errors.Caller(fn(a)))
-//   errs.Append(errors.Caller(fn(b)))
+//   errs.Append(errors.WithCaller(fn(a)))
+//   errs.Append(errors.WithCaller(fn(b)))
 //   err := errs.Err()
 //
 package errors
