f4185346a8d1903de6990e326322b79b5a4a1f96
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2020-09-25 18:08:42 -0700

    .

--- path/go.mod
+++ path/go.mod
@@ -2,4 +2,8 @@ module cloudeng.io/path
 
 go 1.13
 
-require cloudeng.io/errors v0.0.5
+require (
+	cloudeng.io/errors v0.0.5
+	golang.org/x/mod v0.3.0
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+)
---
+++ path/gopkgpath/pkg_path_test.go
@@ -0,0 +1,52 @@
+// Copyright 2020 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package gopkgpath_test
+
+import (
+	"testing"
+
+	"cloudeng.io/path/gopkgpath"
+)
+
+func TestPackagePath(t *testing.T) {
+	p, err := gopkgpath.Caller()
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	if got, want := p, "cloudeng.io/path/gopkgpath"; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	for i, tc := range []struct {
+		depth  int
+		result string
+	}{
+		{0, "cloudeng.io/path/gopkgpath"},
+		{1, "std/testing"},
+		{2, "std/runtime"},
+	} {
+		p, err := gopkgpath.CallerDepth(tc.depth)
+		if err != nil {
+			t.Errorf("%v: %v", i, err)
+		}
+		if got, want := p, tc.result; got != want {
+			t.Errorf("%v: got %v, want %v", i, got, want)
+		}
+	}
+
+	type definedType int
+	for i, tc := range []struct {
+		variable interface{}
+		result   string
+	}{
+		{3, ""},
+		{definedType(3), "cloudeng.io/path/gopkgpath_test"},
+		{gopkgpath.Caller, ""},
+	} {
+		if got, want := gopkgpath.Type(tc.variable), tc.result; got != want {
+			t.Errorf("%v: got %v, want %v", i, got, want)
+		}
+	}
+}
