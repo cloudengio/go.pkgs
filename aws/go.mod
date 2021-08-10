32fde877b9f8ef91fc05b13e53b6d85d413b7030
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2021-08-10 14:42:28 -0700

    webapp: add validate command

--- aws/go.mod
+++ aws/go.mod
@@ -3,14 +3,12 @@ module cloudeng.io/aws
 go 1.16
 
 require (
-	cloudeng.io/cmdutil v0.0.0-20210706232935-3692cd52401d // indirect
-	cloudeng.io/go v0.0.0-20201002213843-cae5e0ddc080 // indirect
-	cloudeng.io/webapp v0.0.0-20210706232935-3692cd52401d // indirect
-	github.com/aws/aws-sdk-go-v2 v1.8.0 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.6.0 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.3.2 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.4.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.5.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.6.1 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	cloudeng.io/cmdutil v0.0.0-20210810185238-8d5e0be9ddb1 // indirect
+	cloudeng.io/webapp v0.0.0-20210810185238-8d5e0be9ddb1
+	github.com/aws/aws-sdk-go-v2 v1.8.0
+	github.com/aws/aws-sdk-go-v2/config v1.6.0
+	github.com/aws/aws-sdk-go-v2/credentials v1.3.2
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.5.0
+	github.com/aws/aws-sdk-go-v2/service/sts v1.6.1
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 )
---
+++ webapp/cmd/acme/redirect.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"cloudeng.io/aws/awscertstore"
+	"cloudeng.io/aws/awsconfig"
+	"cloudeng.io/cmdutil/subcmd"
+	"cloudeng.io/webapp"
+)
+
+type testRedirectFlags struct {
+	webapp.HTTPServerFlags
+	awsconfig.AWSFlags
+}
+
+func redirectCmd() *subcmd.Command {
+	testRedirectCmd := subcmd.NewCommand("redirect-test",
+		subcmd.MustRegisterFlagStruct(&testRedirectFlags{}, nil, nil),
+		testACMERedirect, subcmd.ExactlyNumArguments(0))
+	testRedirectCmd.Document(`test redirecting acme http-01 challenges back to a central server that implements the acme client.`)
+	return testRedirectCmd
+}
+
+func testACMERedirect(ctx context.Context, values interface{}, args []string) error {
+	ctx, done := signal.NotifyContext(ctx, os.Interrupt, os.Kill)
+	defer done()
+	cl := values.(*testRedirectFlags)
+
+	if len(cl.AcmeRedirectTarget) == 0 {
+		return fmt.Errorf("must specific a target for the acme client")
+	}
+
+	if err := webapp.RedirectPort80(ctx, ":443", cl.AcmeRedirectTarget); err != nil {
+		return err
+	}
+
+	storeOpts := []interface{}{}
+	if cl.AWS {
+		cfg, err := awsconfig.Load(ctx)
+		if err != nil {
+			return err
+		}
+		storeOpts = append(storeOpts, awscertstore.WithAWSConfig(cfg))
+	}
+
+	cfg, err := webapp.TLSConfigFromFlags(ctx, cl.HTTPServerFlags, storeOpts...)
+	if err != nil {
+		return err
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello\n")
+	})
+	ln, srv, err := webapp.NewTLSServer(cl.Address, mux, cfg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("listening on: %v\n", ln.Addr())
+	srv.TLSConfig = cfg
+	return webapp.ServeTLSWithShutdown(ctx, ln, srv, time.Minute)
+}
