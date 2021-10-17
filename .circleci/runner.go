64657cb3c768421056f8ed69dfee836c23d6a134
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2021-10-17 13:37:39 -0700

    .

---
+++ .circleci/runner.go
@@ -0,0 +1,94 @@
+// Copyright 2021 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/fs"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+var (
+	prepFlag bool
+	testFlag bool
+)
+
+func main() {
+	ctx := context.Background()
+	flag.BoolVar(&prepFlag, "prep", true, "prepare file system for tests")
+	flag.BoolVar(&testFlag, "test", true, "run tests")
+	flag.Parse()
+
+	if prepFlag {
+		if err := prep(); err != nil {
+			panic(err)
+		}
+	}
+	if testFlag {
+		if err := runTests(ctx); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func prep() error {
+	for _, dir := range []string{
+		filepath.Join("webapp", "cmd", "webapp", "webapp-sample", "build"),
+		filepath.Join("webapp", "cmd", "webapp", "webapp-sample", "build", "static", "css"),
+		filepath.Join("webapp", "cmd", "webapp", "webapp-sample", "build", "static", "js"),
+		filepath.Join("webapp", "cmd", "webapp", "webapp-sample", "build", "static", "media"),
+	} {
+		if err := os.MkdirAll(dir, 0777); err != nil {
+			return err
+		}
+		if err := os.WriteFile(filepath.Join(dir, "dummy-for-embed"), nil, 0666); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func subdirs() ([]string, error) {
+	var dirs []string
+	err := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			if _, err := os.Open(filepath.Join(path, "go.mod")); err == nil {
+				dirs = append(dirs, path)
+				return filepath.SkipDir
+			}
+		}
+		return nil
+	})
+	return dirs, err
+}
+
+func runTests(ctx context.Context) error {
+	dirs, err := subdirs()
+	if err != nil {
+		return err
+	}
+	for _, dir := range dirs {
+		if err := runTest(ctx, dir); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func runTest(ctx context.Context, dir string) error {
+	fmt.Printf("DIR: %v\n", dir)
+	cmd := exec.CommandContext(ctx, "go", "test", "-failfast", "--covermode=atomic", "-race", "./...")
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: build test pullrequest
+.PHONY: build test pr
 
 SUBMODULES = $(wildcard */)
 
@@ -9,40 +9,10 @@ build:
 		cd ..; \
 	done
 
-prep-ci:
-	mkdir -p webapp/cmd/webapp/webapp-sample/build
-	mkdir -p webapp/cmd/webapp/webapp-sample/build/static/css
-	mkdir -p webapp/cmd/webapp/webapp-sample/build/static/js
-	mkdir -p webapp/cmd/webapp/webapp-sample/build/static/media
-	touch webapp/cmd/webapp/webapp-sample/build/dummy-for-embed
-	touch webapp/cmd/webapp/webapp-sample/build/static/css/dummy-for-embed
-	touch webapp/cmd/webapp/webapp-sample/build/static/js/dummy-for-embed
-	touch webapp/cmd/webapp/webapp-sample/build/static/media/dummy-for-embed
-
-test-ci: prep-ci
-	for pkg in $(SUBMODULES); do \
-		cd $$pkg; \
-		go test -failfast --covermode=atomic -race ./...; \
-		cd ..; \
-	done
-
 test:
 	for pkg in $(SUBMODULES); do \
 		cd $$pkg; \
 		go test -v ./...; \
-		cd ..; \
-	done
-
-prep-lint-ci: prep-ci
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
-	go get github.com/matthewloring/validjson/cmd/validjson@latest
-	go install -x github.com/matthewloring/validjson/cmd/validjson@latest
-
-lint-ci:
-	for pkg in $(SUBMODULES); do \
-		cd $$pkg; \
-		golangci-lint run ./...; \
-		validjson ./...; \
 		cd ..; \
 	done
 
@@ -65,4 +35,3 @@ pr:
 		go mod tidy; \
 		cd ..; \
 	done
-
