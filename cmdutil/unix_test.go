40da681ca939dd154b903abc2eed5e708c4d10d3
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2021-10-18 14:23:16 -0700

    .

--- cmdutil/unix_test.go
+++ cmdutil/unix_test.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by the Apache-2.0
 // license that can be found in the LICENSE file.
 
-//go:build darwin
-// +build darwin
+//go:build !windows
+// +build !windows
 
 package cmdutil_test
 
