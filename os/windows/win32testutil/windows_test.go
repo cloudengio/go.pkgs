822ba4e8bc41e03fc7c1179fda7c152d9bca820b
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2021-10-21 09:25:16 -0700

    .

--- os/windows/win32testutil/windows_test.go
+++ os/windows/win32testutil/windows_test.go
@@ -17,6 +17,8 @@ import (
 func TestInaccessible(t *testing.T) {
 	tmpdir := t.TempDir()
 	filename := filepath.Join(tmpdir, "test-file.text")
+
+	// File.
 	if err := os.WriteFile(filename, []byte("hello world\n"), 0666); err != nil {
 		t.Fatal(err)
 	}
@@ -36,8 +38,35 @@ func TestInaccessible(t *testing.T) {
 		t.Errorf("missing or incorrect error: %v", err)
 	}
 
+	if err := MakeAccessibleToOwner(filename); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := os.ReadFile(filename); err != nil {
+		t.Fatal(err)
+	}
+
+	// Directory.
 	dirname := filepath.Join(tmpdir, "test-dir", "sub-dir")
 	if err := os.MkdirAll(dirname, 0777); err != nil {
 		t.Fatal(err)
 	}
+
+	if err := MakeInaccessibleToOwner(dirname); err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = os.ReadDir(dirname)
+	if err == nil || !strings.Contains(err.Error(), "Access is denied") {
+		t.Errorf("missing or incorrect error: %v", err)
+	}
+
+	if err := MakeAccessibleToOwner(dirname); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := os.ReadDir(dirname); err != nil {
+		t.Fatal(err)
+	}
+
 }
