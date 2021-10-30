f6752a638213f2edccd443875fef208cae43a0c8
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2021-10-29 18:38:26 -0700

    .

---
+++ sys/go.mod
@@ -0,0 +1,5 @@
+module cloudeng.io/sys
+
+go 1.16
+
+require golang.org/x/sys v0.0.0-20190412213103-97732733099d
---
+++ sys/windows/powershell/powershell_test.go
@@ -0,0 +1,34 @@
+// Copyright 2021 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+//go:build windows
+// +build windows
+
+package powershell_test
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"cloudeng.io/sys/windows/powershell"
+)
+
+func TestSimple(t *testing.T) {
+	ps := powershell.New()
+	stdout, stderr, err := ps.Run(`$env:username`)
+	if err != nil {
+		t.Fatalf("failed: %v %v", stderr, err)
+	}
+	stdout = strings.TrimSpace(stdout)
+	t.Log(stdout)
+	if len(stdout) < 2 {
+		t.Errorf("looks too small to be a valid user name)")
+	}
+	if os.Getenv("CIRCLECI") == "true" {
+		if got, want := stdout, "circleci"; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
