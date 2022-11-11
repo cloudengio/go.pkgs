1194ad6629e43602c9431b6e5829ffff6b70cb08
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2022-11-11 11:16:52 -0800

    cmdutil/subcmd: allow command tree to be specified in yaml. (#88)
    This PR provides for more readable and concise definition of command trees by using a YAML specification for defining the command tree, documentation and expected arguments for each command.

---
+++ cmdutil/subcmd/testdata/toplevel.go
@@ -0,0 +1,44 @@
+// Copyright 2022 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"cloudeng.io/cmdutil/subcmd"
+)
+
+const simple = `name: simple
+usage: 
+`
+
+var cmdSet *subcmd.CommandSet
+
+type simpleFlags struct {
+	Flag1 int `subcmd:"flag1,1,flag1"`
+	Flag2 int `subcmd:"flag2,2,flag2"`
+}
+
+func init() {
+	toplevel := subcmd.NewCommand(
+		"simple",
+		subcmd.MustRegisteredFlagSet(&simpleFlags{}),
+		runner,
+		subcmd.WithoutArguments())
+	toplevel.Document("overall documentation for the simple command")
+	cmdSet = subcmd.NewCommandSet()
+	cmdSet.TopLevel(toplevel)
+}
+
+func runner(ctx context.Context, values interface{}, args []string) error {
+	fv := values.(*simpleFlags)
+	fmt.Printf("runner: flag values: %v %v\n", fv.Flag1, fv.Flag2)
+	return nil
+}
+
+func main() {
+	cmdSet.MustDispatch(context.Background())
+}
