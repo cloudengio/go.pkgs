32d7549d64ffd3a4d10433a4e79a3209f4119dfe
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2022-11-17 19:13:04 -0800

    .

---
+++ cmdutil/subcmd/yaml_internal_test.go
@@ -0,0 +1,21 @@
+// Copyright 2022 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package subcmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func (c *CommandSetYAML) Usage(names ...string) string {
+	pathname := strings.Join(names, "/")
+	if cmd := c.cmdDict[pathname]; cmd != nil {
+		return cmd.Usage()
+	}
+	fmt.Printf("d: %v\n", c.cmdDict)
+	fmt.Printf(">>> %v\n", pathname)
+	return c.CommandSet.Usage(os.Args[0])
+}
