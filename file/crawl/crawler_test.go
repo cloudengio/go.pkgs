c6d46ca50929893bd2f0cf60acf335d4126f74a3
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-01-09 14:57:15 -0800

    file/crawl: add multilevel crawling (#102)

---
+++ file/crawl/crawler_test.go
@@ -0,0 +1,213 @@
+// Copyright 2022 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package crawl_test
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"cloudeng.io/file"
+	"cloudeng.io/file/crawl"
+	"cloudeng.io/file/download"
+	"cloudeng.io/file/filetestutil"
+	"cloudeng.io/sync/synctestutil"
+)
+
+type crawlRequest struct {
+	container fs.FS
+	names     []string
+	depth     int
+}
+
+func (cr *crawlRequest) Container() fs.FS {
+	return cr.container
+}
+
+func (cr *crawlRequest) Names() []string {
+	return cr.names
+}
+
+func (cr crawlRequest) FileMode() fs.FileMode {
+	return fs.FileMode(0600)
+}
+
+type extractor struct {
+	sync.Mutex
+	fanOut int
+}
+
+func (e *extractor) Extract(ctx context.Context, depth int, downloaded download.Downloaded) []download.Request {
+	e.Lock()
+	defer e.Unlock()
+	outlinks := (*downloaded.Request.(*crawlRequest))
+	outlinks.names = nil
+	for _, dlr := range downloaded.Downloads {
+		for nout := 0; nout < e.fanOut; nout++ {
+			outlinks.names = append(outlinks.names, dlr.Name+fmt.Sprintf("-%02v", nout))
+		}
+	}
+	return []download.Request{&outlinks}
+}
+
+func issuseCrawlRequests(ctx context.Context, nItems int, input chan<- download.Request, reader fs.FS) {
+	for i := 0; i < nItems; i++ {
+		select {
+		case input <- &crawlRequest{container: reader, depth: 0, names: []string{fmt.Sprintf("%08v", i)}}:
+		case <-ctx.Done():
+			break
+		}
+	}
+	close(input)
+}
+
+type dlFactory struct {
+	numDownloaders int
+}
+
+func (df dlFactory) create(ctx context.Context, depth int) (
+	downloader download.T,
+	inputCh chan download.Request,
+	outputCh chan download.Downloaded) {
+	concurrency := 0
+	chanCap := 0
+	// Use conservative channel capacities to ensure that blocking
+	// is encountered within the crawler.
+	switch {
+	case depth == 0:
+		concurrency = 2
+		chanCap = 1
+	case depth < 6:
+		concurrency = depth * 2
+		chanCap = depth
+	default:
+		chanCap = 100
+	}
+	inputCh = make(chan download.Request, chanCap)
+	outputCh = make(chan download.Downloaded, chanCap)
+	downloader = download.New(
+		download.WithNumDownloaders(concurrency),
+		download.WithNumDownloaders(df.numDownloaders))
+	return
+}
+
+func expectedFilenames(nItems, depth, fanOut int) []string {
+	names := []string{}
+	for i := 0; i < nItems; i++ {
+		names = append(names, fmt.Sprintf("%08v", i))
+	}
+	prev := names
+	for d := 1; d <= depth; d++ {
+		extracted := []string{}
+		for _, p := range prev {
+			for f := 0; f < fanOut; f++ {
+				extracted = append(extracted, p+fmt.Sprintf("-%02v", f))
+			}
+		}
+		prev = extracted
+		names = append(names, extracted...)
+	}
+	return names
+}
+
+func TestCrawler(t *testing.T) {
+	defer synctestutil.AssertNoGoroutines(t)()
+
+	ctx := context.Background()
+	src := rand.NewSource(time.Now().UnixMicro())
+
+	nItems := 100
+	fanOut := 2 // number of outlinks per download.
+
+	for _, depth := range []int{0, 1, 4} {
+		readFS := filetestutil.NewMockFS(filetestutil.FSWithConstantContents([]byte{'a', 'b'}, 100))
+		readFS = filetestutil.NewMockFS(filetestutil.FSWithRandomContents(src, 1024))
+		writeFS := filetestutil.NewMockFS(filetestutil.FSWriteFS()).(file.WriteFS)
+
+		inputCh := make(chan download.Request, 10)
+		outputCh := make(chan crawl.Crawled, 10)
+
+		crawler := crawl.New(
+			crawl.WithNumExtractors(2),
+			crawl.WithCrawlDepth(depth))
+
+		errCh := make(chan error, 1)
+		wg := &sync.WaitGroup{}
+		wg.Add(3)
+
+		outlinks := &extractor{fanOut: fanOut}
+
+		df := &dlFactory{numDownloaders: 1}
+		go func() {
+			errCh <- crawler.Run(ctx, df.create, outlinks, writeFS, inputCh, outputCh)
+			wg.Done()
+		}()
+
+		go func() {
+			issuseCrawlRequests(ctx, nItems, inputCh, readFS)
+			wg.Done()
+		}()
+
+		crawled := []crawl.Crawled{}
+		nDownloads := 0
+		nOutlinks := 0
+		go func() {
+			for outs := range outputCh {
+				crawled = append(crawled, outs)
+				nDownloads += len(outs.Downloads)
+				for _, ol := range outs.Outlinks {
+					nOutlinks += len(ol.Names())
+				}
+			}
+			wg.Done()
+		}()
+
+		wg.Wait()
+
+		if err := <-errCh; err != nil {
+			t.Fatal(err)
+		}
+
+		expectedDownloads := nItems
+		prev := nItems
+		for d := 0; d < depth; d++ {
+			prev = prev * fanOut
+			expectedDownloads += prev
+		}
+		expectedOutlinks := expectedDownloads * fanOut
+
+		t.Logf("depth %v, expected downloads: %v, expected outlinks %v", depth, expectedDownloads, expectedOutlinks)
+
+		if got, want := nDownloads, expectedDownloads; got != want {
+			t.Errorf("depth %v: got %v, want %v", depth, got, want)
+		}
+
+		if got, want := nOutlinks, expectedOutlinks; got != want {
+			t.Errorf("depth %v: got %v, want %v", depth, got, want)
+		}
+
+		crawledContents := filetestutil.Contents(writeFS)
+		if got, want := len(crawledContents), expectedDownloads; got != want {
+			t.Errorf("depth %v: got %v, want %v", depth, got, want)
+		}
+
+		expectedDownloadNames := expectedFilenames(nItems, depth, fanOut)
+		if got, want := len(expectedDownloadNames), expectedDownloads; got != want {
+			t.Errorf("depth %v: got %v, want %v", depth, got, want)
+		}
+		for _, k := range expectedDownloadNames {
+			if _, ok := crawledContents[k]; !ok {
+				t.Errorf("%v was not crawled", k)
+			}
+		}
+		if err := filetestutil.CompareFS(readFS, writeFS); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
---
+++ file/crawl/extractors.go
@@ -0,0 +1,69 @@
+// Copyright 2023 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package crawl
+
+import (
+	"context"
+
+	"cloudeng.io/file/download"
+	"cloudeng.io/sync/errgroup"
+)
+
+type extractorPool struct {
+	outlinks           Outlinks
+	depth, concurrency int
+}
+
+// export this when everything is working.
+func newExtractorPool(outlinks Outlinks, depth, concurrency int) *extractorPool {
+	return &extractorPool{
+		outlinks:    outlinks,
+		depth:       depth,
+		concurrency: concurrency,
+	}
+}
+
+func (ep *extractorPool) run(ctx context.Context, downloadedCh <-chan download.Downloaded, crawledCh chan<- Crawled) error {
+	// Extractors return when extractorStop is closed.
+	var grp errgroup.T
+	for i := 0; i < ep.concurrency; i++ {
+		i := i
+		grp.Go(func() error {
+			ep.runExtractor(ctx, i, downloadedCh, crawledCh)
+			return nil
+		})
+	}
+	return grp.Wait()
+}
+
+func (ep *extractorPool) runExtractor(ctx context.Context,
+	id int,
+	downloadedCh <-chan download.Downloaded,
+	crawledCh chan<- Crawled) {
+	for {
+		// Wait for newly downloaded items.
+		var downloaded download.Downloaded
+		var ok bool
+		select {
+		case <-ctx.Done():
+			return
+		case downloaded, ok = <-downloadedCh:
+			if !ok {
+				return
+			}
+		}
+		crawled := Crawled{Depth: ep.depth, Downloaded: downloaded}
+		// Extract outlinks.
+		extracted := ep.outlinks.Extract(ctx, ep.depth, crawled.Downloaded)
+		for _, outlinks := range extracted {
+			crawled.Outlinks = append(crawled.Outlinks, outlinks)
+		}
+		select {
+		case <-ctx.Done():
+			return
+		case crawledCh <- crawled:
+		}
+	}
+}
