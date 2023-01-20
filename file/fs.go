aafb8aeef9d36673e48bf200db5715d9a595c4fd
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-01-20 10:14:10 -0800

    file: add fs.FileInfo implementation with gob/json encoding/decoding

--- file/fs.go
+++ file/fs.go
@@ -5,9 +5,17 @@
 package file
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
+	"encoding/json"
 	"io/fs"
+	"time"
 )
+
+func init() {
+	gob.Register(&Info{})
+}
 
 // FS extends fs.FS with OpenCtx.
 type FS interface {
@@ -26,4 +34,113 @@ type fsFromFS struct {
 
 func (f *fsFromFS) OpenCtx(ctx context.Context, name string) (fs.File, error) {
 	return f.Open(name)
+}
+
+// Info implements fs.FileInfo with gob and json encoding/decoding. Note that
+// the Sys value is not encoded/decode and is only avalilable within the
+// process that originally created the info Instance.
+type Info struct {
+	name    string
+	size    int64
+	mode    fs.FileMode
+	modTime time.Time
+	isDir   bool
+	sysInfo interface{}
+}
+
+// NewInfo creates a new instance of Info.
+func NewInfo(name string, size int64, mode fs.FileMode, mod time.Time, dir bool, sys interface{}) *Info {
+	return &Info{
+		name:    name,
+		size:    size,
+		mode:    mode,
+		modTime: mod,
+		isDir:   dir,
+		sysInfo: sys,
+	}
+}
+
+func (fi *Info) Name() string {
+	return fi.name
+}
+
+func (fi *Info) Size() int64 {
+	return fi.size
+}
+
+func (fi *Info) Mode() fs.FileMode {
+	return fi.mode
+}
+
+func (fi *Info) ModTime() time.Time {
+	return fi.modTime
+}
+
+func (fi *Info) IsDir() bool {
+	return fi.isDir
+}
+
+func (fi *Info) Sys() interface{} {
+	return fi.sysInfo
+}
+
+func (fi *Info) asInfo() info {
+	return info{
+		Filename:    fi.name,
+		FileSize:    fi.size,
+		FileMode:    fi.mode,
+		FileModTime: fi.modTime,
+		FileIsDir:   fi.isDir,
+	}
+}
+
+func (fi *Info) fromInfo(i info) {
+	fi.name = i.Filename
+	fi.size = i.FileSize
+	fi.mode = i.FileMode
+	fi.modTime = i.FileModTime
+	fi.isDir = i.FileIsDir
+}
+
+func (fi *Info) GobEncode() ([]byte, error) {
+	tmp := fi.asInfo()
+	buf := &bytes.Buffer{}
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(tmp); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func (fi *Info) MarshalJSON() ([]byte, error) {
+	return json.Marshal(fi.asInfo())
+}
+
+func (fi *Info) GobDecode(data []byte) error {
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	var tmp info
+	if err := dec.Decode(&tmp); err != nil {
+		return err
+	}
+	fi.fromInfo(tmp)
+	return nil
+}
+
+func (fi *Info) UnmarshalJSON(data []byte) error {
+	var tmp info
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+	fi.fromInfo(tmp)
+	return nil
+}
+
+// info is like Info but without the Sys field.
+type info struct {
+	Filename    string
+	FileSize    int64
+	FileMode    fs.FileMode
+	FileModTime time.Time
+	FileIsDir   bool
 }
---
+++ file/fs_test.go
@@ -0,0 +1,77 @@
+// Copyright 2023 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+// Package s3fs implements fs.FS for AWS S3.
+package file_test
+
+import (
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
+	"io/fs"
+	"testing"
+	"time"
+
+	"cloudeng.io/file"
+)
+
+func gobRoundTrip(t *testing.T, fi *file.Info) file.Info {
+	buf := &bytes.Buffer{}
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(fi); err != nil {
+		t.Fatal(err)
+	}
+	var nfi file.Info
+	dec := gob.NewDecoder(buf)
+	if err := dec.Decode(&nfi); err != nil {
+		t.Fatal(err)
+	}
+	return nfi
+}
+
+func jsonRoundTrip(t *testing.T, fi *file.Info) file.Info {
+	buf, err := json.Marshal(fi)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var nfi file.Info
+	if err := json.Unmarshal(buf, &nfi); err != nil {
+		t.Fatal(err)
+	}
+	return nfi
+}
+
+func TestEncodeDecode(t *testing.T) {
+
+	sysinfo := struct{ name string }{"foo"}
+
+	now := time.Now().Round(0) // strip the monotonic clock value.
+	fi := file.NewInfo("ab", 32, 0700, now, true, &sysinfo)
+
+	type roundTripper func(*testing.T, *file.Info) file.Info
+
+	for _, fn := range []roundTripper{
+		jsonRoundTrip, gobRoundTrip,
+	} {
+		nfi := fn(t, fi)
+		if got, want := nfi.Name(), "ab"; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+		if got, want := nfi.Size(), int64(32); got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+		if got, want := nfi.Mode(), fs.FileMode(0700); got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+		if got, want := nfi.ModTime(), now; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+		if got, want := nfi.IsDir(), true; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+		if got, want := nfi.Sys(), any(nil); got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
