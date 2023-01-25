153086ef6388fc26210c3665b6bb1d9ada2cf746
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-01-24 17:53:29 -0800

    file/crawl: add support to simplify building command line tools for crawling

--- file/crawl/crawlcmd/crawlcfg.go
+++ file/crawl/crawlcmd/crawlcfg.go
@@ -2,9 +2,20 @@
 // Use of this source code is governed by the Apache-2.0
 // license that can be found in the LICENSE file.
 
+// Package crawlcmd provides support for building command line tools for
+// crawling. In particular it provides support for managing the configuration
+// of a crawl via yaml.
 package crawlcmd
 
-import "cloudeng.io/file/content"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"cloudeng.io/file/content"
+	"cloudeng.io/file/crawl/outlinks"
+	"cloudeng.io/path/cloudpath"
+)
 
 // DownloadFactoryConfig is the configuration for a crawl.DownloaderFactory.
 type DownloadFactoryConfig struct {
@@ -20,20 +31,69 @@ type DownloadFactoryConfig struct {
 // will be used to store the results of the crawl. The cache is intended
 // to be relative to the
 type CrawlCacheConfig struct {
-	CachePrefix           string `yaml:"cache_prefix"`
-	CacheClearBeforeCrawl bool   `yaml:"cache_clear_before_crawl"`
+	Prefix           string `yaml:"cache_prefix"`
+	ClearBeforeCrawl bool   `yaml:"cache_clear_before_crawl"`
 }
 
 // Confiug represents the configuration for a single crawl.
 type Config struct {
-	Name          string                  `yaml:"name"`
-	Depth         int                     `yaml:"depth"`
-	Seeds         []string                `yaml:"seeds"`
-	NoFollowRules []string                `yaml:"nofollow"`
-	FollowRules   []string                `yaml:"follow"`
-	RewriteRules  []string                `yaml:"rewrite"`
-	Download      DownloadFactoryConfig   `yaml:"download"`
-	NumExtractors int                     `yaml:"num_extractors"`
-	Extractors    map[content.Type]string `yaml:"extractors"`
-	Cache         CrawlCacheConfig        `yaml:"cache"`
+	Name          string                `yaml:"name"`
+	Depth         int                   `yaml:"depth"`
+	Seeds         []string              `yaml:"seeds"`
+	NoFollowRules []string              `yaml:"nofollow"`
+	FollowRules   []string              `yaml:"follow"`
+	RewriteRules  []string              `yaml:"rewrite"`
+	Download      DownloadFactoryConfig `yaml:"download"`
+	NumExtractors int                   `yaml:"num_extractors"`
+	Extractors    []content.Type        `yaml:"extractors"`
+	Cache         CrawlCacheConfig      `yaml:"cache"`
+}
+
+// NewLinkProcessor creates a outlinks.RegexpProcessor using the
+// nofollow, follow and reqwrite specifications in the configuration.
+func (c Config) NewLinkProcessor() (*outlinks.RegexpProcessor, error) {
+	linkProcessor := &outlinks.RegexpProcessor{
+		NoFollow: c.NoFollowRules,
+		Follow:   c.FollowRules,
+		Rewrite:  c.RewriteRules,
+	}
+	if err := linkProcessor.Compile(); err != nil {
+		return nil, fmt.Errorf("failed to compile link processing rules: %v", err)
+	}
+	return linkProcessor, nil
+}
+
+// CreateAndCleanCache creates the cache directory for the crawl, relative
+// to the specified root, and optionally clears it before the crawl (if
+// Cache.ClearBeforeCrawl is true). Any environment variables in the
+// root or Cache.Prefix will be expanded.
+func (c Config) CreateAndCleanCache(root string) error {
+	if len(c.Cache.Prefix) == 0 {
+		return nil
+	}
+	root = os.ExpandEnv(root)
+	crawlCache := filepath.Join(root, os.ExpandEnv(c.Cache.Prefix))
+	if c.Cache.ClearBeforeCrawl {
+		if err := os.RemoveAll(crawlCache); err != nil {
+			return fmt.Errorf("failed to remove %v: %v", crawlCache, err)
+		}
+	}
+	return nil
+}
+
+// SeedsByScheme returns the crawl seeds grouped by their scheme and any seeds
+// that are not recognised by the supplied cloudpath.MatcherSpec.
+func (c Config) SeedsByScheme(matchers cloudpath.MatcherSpec) (map[string][]cloudpath.Match, []string) {
+	matches := map[string][]cloudpath.Match{}
+	rejected := []string{}
+	for _, seed := range c.Seeds {
+		match := matchers.Match(seed)
+		if len(match.Matched) == 0 {
+			rejected = append(rejected, seed)
+			continue
+		}
+		scheme := match.Scheme
+		matches[scheme] = append(matches[scheme], match)
+	}
+	return matches, rejected
 }
--- file/crawl/crawlcmd/crawlcfg_test.go
+++ file/crawl/crawlcmd/crawlcfg_test.go
@@ -10,6 +10,7 @@ import (
 
 	"cloudeng.io/file/content"
 	"cloudeng.io/file/crawl/crawlcmd"
+	"cloudeng.io/path/cloudpath"
 	"gopkg.in/yaml.v3"
 )
 
@@ -26,9 +27,7 @@ const crawlsSpec = `
     default_downloads_chan_size: 100
     per_depth_concurrency: [1, 2, 4]
   num_extractors: 3
-  extractors:
-    text/html;charset=utf-8: html
-    text/plain;charset=utf-8: text
+  extractors: [text/html;charset=utf-8, text/plain;charset=utf-8]
   `
 
 func TestCrawlConfig(t *testing.T) {
@@ -61,7 +60,35 @@ func TestCrawlConfig(t *testing.T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 
-	if got, want := crawl.Extractors, map[content.Type]string{"text/html;charset=utf-8": "html", "text/plain;charset=utf-8": "text"}; !reflect.DeepEqual(got, want) {
+	if got, want := crawl.Extractors, []content.Type{"text/html;charset=utf-8", "text/plain;charset=utf-8"}; !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestSeedSorting(t *testing.T) {
+	var crawl crawlcmd.Config
+	crawl.Seeds = []string{"https://yahoo.com", "s3://foo/bar", "s3://baz", "c:/foo/bar"}
+
+	byScheme, rejected := crawl.SeedsByScheme(cloudpath.DefaultMatchers)
+	if got, want := len(byScheme), 3; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	if got, want := len(rejected), 0; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	s3Only := []cloudpath.Matcher{cloudpath.AWSS3Matcher}
+	byScheme, rejected = crawl.SeedsByScheme(s3Only)
+	if got, want := len(byScheme), 1; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	if got, want := len(byScheme["s3"]), 2; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	if got, want := rejected, []string{"https://yahoo.com", "c:/foo/bar"}; !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
