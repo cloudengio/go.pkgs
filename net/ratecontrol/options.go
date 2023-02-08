f54060b0088a0987e51d2764960f91ead5545d32
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-02-08 10:55:10 -0800

    net/ratecontrol: package to implement rate control and back off etc. (#149)

---
+++ net/ratecontrol/options.go
@@ -0,0 +1,97 @@
+// Copyright 2023 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+// Package ratecontrol provides mechanisms for controlling the rate
+// at which requests are made and for backing off when the remote
+// service is unwilling to process requests.
+package ratecontrol
+
+import "time"
+
+// Clock represents a clock used for rate limiting. It determines the current
+// time in 'ticks' and the wall-clock duration of a tick. This allows for
+// rates to be specified in terms of requests per tick or bytes per tick rather
+// than over a fixed duration. A default Clock implementation is provided
+// which uses time.Minute as the tick duration.
+type Clock interface {
+	Tick() int
+	TickDuration() time.Duration
+
+	// for testing.
+	after(d time.Duration) <-chan time.Time
+}
+
+type clock struct{}
+
+// Tick returns the current tick which will increment every TickDuration()
+func (c clock) Tick() int {
+	return time.Now().Minute()
+}
+
+// TickDuration returns the duration of a tick.
+func (c clock) TickDuration() time.Duration {
+	return time.Minute
+}
+
+// after returns a channel as per timer.After, it is used for testing.
+func (c clock) after(d time.Duration) <-chan time.Time {
+	return time.After(d)
+}
+
+// Option represents an option for configuring a ratecontrol Controller.
+type Option func(c *options)
+
+// WithRequestsPerTick sets the rate for requests in requests per tick,
+// where tick is the unit of time reported by the Clock implementation in use.
+// The default clock uses time.Now().Minute() as the interval for rate limiting
+// and hence the rate is in requests per minute.
+func WithRequestsPerTick(rpt int) Option {
+	return func(o *options) {
+		o.reqsPerTick = rpt
+	}
+}
+
+// WithBytesPerTick sets the approximate rate in bytes per tick, where
+// tick is the unit of time reported by the Clock implementation
+// in use. The default clock uses time.Now().Minute() and hence the rate
+// is in bytes per minute. The algorithm used is very simple and will
+// wait for a single tick if the limit is reached without taking into account
+// how long the tick is, nor how much excess data was sent over the
+// previous tick (ie. no attempt is made to smooth out the rate and for now
+// it's a simple start/stop model). The bytes to be accounted for are
+// reported to the Controller via its BytesTransferred method.
+func WithBytesPerTick(bpt int) Option {
+	return func(o *options) {
+		o.bytesPerTick = bpt
+	}
+}
+
+// WithBackoffParameters enables an exponential backoff algorithm that
+// is triggered when the download fails in a way that is retryable. The
+// container (fs.FS) implementation must return an error that returns
+// true for errors.Is(err, retryErr). First defines the first backoff delay,
+// which is then doubled for every consecutive retry until the
+// download either succeeds or the specified number of steps (attempted
+// downloads) is exceeded (the download is then deemed to have failed).
+func WithBackoffParameters(first time.Duration, steps int) Option {
+	return func(o *options) {
+		o.backoffStart = first
+		o.backoffSteps = steps
+	}
+}
+
+// WithClock sets the clock implementation to use.
+func WithClock(c Clock) Option {
+	return func(o *options) {
+		o.clock = c
+	}
+}
+
+type options struct {
+	reqsPerTick  int
+	bytesPerTick int
+	backoffStart time.Duration
+	backoffSteps int
+	clock        Clock
+}
---
+++ net/ratecontrol/ratecontrol_test.go
@@ -0,0 +1,142 @@
+// Copyright 2023 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package ratecontrol_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"cloudeng.io/net/ratecontrol"
+)
+
+func TestNoop(t *testing.T) {
+	ctx := context.Background()
+	clk := &ratecontrol.TestClock{}
+	c := ratecontrol.New(ratecontrol.WithClock(clk))
+	for i := 0; i < 100; i++ {
+		if err := c.Wait(ctx); err != nil {
+			t.Fatal(err)
+		}
+		done, err := c.Backoff(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got, want := done, true; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+	if got, want := clk.Called, 0; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestRequestRate(t *testing.T) {
+	ctx := context.Background()
+	clk := &ratecontrol.TestClock{TickDurationValue: 10 * time.Millisecond}
+	c := ratecontrol.New(ratecontrol.WithClock(clk),
+		ratecontrol.WithRequestsPerTick(1))
+	now := time.Now()
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Millisecond * 9)
+		if err := c.Wait(ctx); err != nil {
+			t.Fatal(err)
+		}
+	}
+	since := time.Since(now)
+	// tighter lower bound than upper bound since the former
+	// will be due to clock granularity issues and the latter to
+	// a slow machine which is common on CI systems.
+	lower, upper := 90*time.Millisecond, 200*time.Millisecond
+	if got := since; got < lower || got > upper {
+		t.Errorf("wait delay: %v not in range %v..%v", got, lower, upper)
+	}
+	if got, want := clk.Called, 1; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestDataRate(t *testing.T) {
+	ctx := context.Background()
+	clk := &ratecontrol.TestClock{AfterValue: time.Millisecond, TickDurationValue: 10 * time.Millisecond}
+	c := ratecontrol.New(ratecontrol.WithClock(clk),
+		ratecontrol.WithBytesPerTick(10))
+	for i := 0; i < 10; i++ {
+		if err := c.Wait(ctx); err != nil {
+			t.Fatal(err)
+		}
+		c.BytesTransferred(100)
+		if got, want := len(clk.AfterDurations), i; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func backoff(ctx context.Context, t *testing.T, c *ratecontrol.Controller) int {
+	c.InitBackoff()
+	for {
+		done, err := c.Backoff(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if done {
+			return c.Retries()
+		}
+	}
+}
+
+func TestBackoff(t *testing.T) {
+	ctx := context.Background()
+	// return immediately on retry
+	clk := &ratecontrol.TestClock{AfterValue: time.Nanosecond}
+	numRetries := 10
+	c := ratecontrol.New(ratecontrol.WithClock(clk),
+		ratecontrol.WithBackoffParameters(time.Millisecond, numRetries),
+	)
+	for i := 0; i < 3; i++ {
+		retries := backoff(ctx, t, c)
+		if got, want := retries, numRetries; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func TestCancel(t *testing.T) {
+	rootCtx := context.Background()
+	ctx, cancel := context.WithCancel(rootCtx)
+	clk := &ratecontrol.TestClock{AfterValue: time.Hour, TickDurationValue: time.Hour}
+	c := ratecontrol.New(ratecontrol.WithClock(clk),
+		ratecontrol.WithBackoffParameters(time.Hour, 10),
+		ratecontrol.WithBytesPerTick(10),
+		ratecontrol.WithRequestsPerTick(1),
+	)
+	go cancel()
+	err := c.Wait(ctx)
+	if err == nil || err != context.Canceled {
+		t.Errorf("got %v, want %v", err, context.Canceled)
+	}
+
+	ctx, cancel = context.WithCancel(rootCtx)
+	go cancel()
+	last, err := c.Backoff(ctx)
+
+	if got, want := last, true; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	if err == nil || err != context.Canceled {
+		t.Errorf("got %v, want %v", err, context.Canceled)
+	}
+
+	c = ratecontrol.New(ratecontrol.WithClock(clk),
+		ratecontrol.WithBytesPerTick(10))
+	ctx, cancel = context.WithCancel(rootCtx)
+	c.BytesTransferred(1000)
+	go cancel()
+
+	err = c.Wait(ctx)
+	if err == nil || err != context.Canceled {
+		t.Errorf("got %v, want %v", err, context.Canceled)
+	}
+}
