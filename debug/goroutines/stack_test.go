e4a5e0a2d149ac0bde902b8646c6722eb6e2dffd
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-09-20 14:59:43 -0700

    debug/goroutines: update for go1.21

--- debug/goroutines/stack_test.go
+++ debug/goroutines/stack_test.go
@@ -67,9 +67,18 @@ func TestGet(t *testing.T) {
 		bycreator[key] = g
 	}
 
+	findGoroutine := func(caller string) *goroutines.Goroutine {
+		for _, g := range gs {
+			if g.Creator != nil && strings.HasPrefix(g.Creator.Call, caller) {
+				return g
+			}
+		}
+		return nil
+	}
+
 	pkgPath, _ := gopkgpath.Caller()
 	pkgPath += "_test."
-	a := bycreator[pkgPath+"runGoA"]
+	a := findGoroutine(pkgPath + "runGoA")
 	switch {
 	case a == nil:
 		for _, g := range gs {
@@ -78,22 +87,25 @@ func TestGet(t *testing.T) {
 			}
 		}
 		fmt.Printf("><>< %v\n", bycreator)
-		panic("runGoA is missing")
+		for k, v := range bycreator {
+			fmt.Printf(": %v: %v\n", k, v)
+		}
+		panic(pkgPath + "runGoA is missing")
 	case len(a.Stack) < 1:
 		t.Errorf("got %d expected at least 1: %s", len(a.Stack), goroutines.Format(a))
 	case !strings.HasPrefix(a.Stack[0].Call, pkgPath+"waitForIt"):
 		t.Errorf("got %s, wanted it to start with %swaitForIt",
 			a.Stack[0].Call, pkgPath)
 	}
-	b := bycreator[pkgPath+"runGoB"]
+	b := findGoroutine(pkgPath + "runGoB")
 	if b == nil {
-		t.Errorf("runGoB is missing")
+		t.Errorf("%srunGoB is missing", pkgPath)
 	} else if len(b.Stack) < 5 {
 		t.Errorf("got %d expected at least 5: %s", len(b.Stack), goroutines.Format(b))
 	}
-	c := bycreator[pkgPath+"runGoC"]
+	c := findGoroutine(pkgPath + "runGoC")
 	if c == nil {
-		t.Errorf("runGoC is missing")
+		t.Errorf("%srunGoC is missing", pkgPath)
 	} else if len(c.Stack) < 1 {
 		t.Errorf("got %d expected at least 1: %s", len(c.Stack), goroutines.Format(c))
 	}
@@ -122,14 +134,23 @@ func TestGetIgnore(t *testing.T) {
 		bycreator[key] = g
 	}
 
+	findGoroutine := func(caller string) bool {
+		for _, g := range gs {
+			if g.Creator != nil && strings.HasPrefix(g.Creator.Call, caller) {
+				return true
+			}
+		}
+		return false
+	}
+
 	pkgPath, _ := gopkgpath.Caller()
 	pkgPath += "_test."
 	for _, ignored := range []string{"runGoA", "runGoB"} {
-		if _, ok := bycreator[pkgPath+ignored]; ok {
+		if findGoroutine(pkgPath + ignored) {
 			t.Errorf("%v should have been recorded", ignored)
 		}
 	}
-	if _, ok := bycreator[pkgPath+"runGoC"]; !ok {
+	if !findGoroutine(pkgPath + "runGoC") {
 		t.Errorf("%v should have been recorded", "runGoC")
 	}
 }
