c33c381aab832071a353d1de81ae3203782e5db8
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-09-20 11:29:25 -0700

    .

--- cmdutil/flags/time_test.go
+++ cmdutil/flags/time_test.go
@@ -6,20 +6,35 @@ package flags_test
 
 import (
 	"testing"
+	"time"
 
 	"cloudeng.io/cmdutil/flags"
 )
 
 func TestTimeFlag(t *testing.T) {
-	tf := &flags.Time{}
-	for i, tc := range []string{
-		"2021-10-10",                // date only
-		"2021-10-10T03:03:03-07:00", // RFC 3339
-		"03:03:05",                  // time only
-		"2021-10-10 03:03:05",       // date time
+	tp := func(f, v string) time.Time {
+		tv, err := time.Parse(f, v)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return tv
+	}
+
+	for i, tc := range []struct {
+		in     string
+		format string
+	}{
+		{"2021-10-10", time.DateOnly},
+		{"2021-10-10T03:03:03-07:00", time.RFC3339},
+		{"03:03:05", time.TimeOnly},
+		{"2021-10-10 03:03:05", time.DateTime},
 	} {
-		if err := tf.Set(tc); err != nil {
+		tf := &flags.Time{}
+		if err := tf.Set(tc.in); err != nil {
 			t.Errorf("%v: %v", i, err)
+		}
+		if got, want := tf.Get().(time.Time), tp(tc.format, tc.in); !got.Equal(want) {
+			t.Errorf("got %v, want %v", got, want)
 		}
 	}
 }
