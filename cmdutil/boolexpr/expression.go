7ba62a198c99add1a919a1b3cc4bf1de79633cee
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-12-02 17:05:31 -0800

    cmdutil/boolexpr: move from file/matcher and filewalk/find to cmdutil (#228)

---
+++ cmdutil/boolexpr/expression.go
@@ -0,0 +1,278 @@
+// Copyright 2023 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+// Package boolexpr provides a boolean expression evaluator and parser.
+// The supported operators are && and ||, and grouping via ().
+// The set of operands is defined by clients of the package by
+// implementing the Operand interface. Operands represent simple predicates
+// against which the value supplied to the expression is evaluated, as such,
+// they implicitly contain a value of their own that is assigned when the
+// operand is instantiated. For example, a simple string comparison operand
+// would be represented as "name='foo' || name='bar'" which evaluated to true
+// if the expression is evaluated for "foo" or "bar", but not otherwise.
+package boolexpr
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type itemType int
+
+const (
+	orOp itemType = iota
+	andOp
+	leftBracket
+	rightBracket
+	subExpression
+	operand
+)
+
+func (it itemType) String() string {
+	switch it {
+	case andOp:
+		return "&&"
+	case orOp:
+		return "||"
+	case leftBracket:
+		return "("
+	case rightBracket:
+		return ")"
+	case operand:
+		return "operand"
+	case subExpression:
+		return "(...)"
+	default:
+		return fmt.Sprintf("unknown item type: %d", it)
+	}
+}
+
+// Item represents an operator or operand in an expression. It is exposed
+// to allow clients packages to create their own parsers.
+type Item struct {
+	typ itemType
+	sub subItems
+	op  Operand
+}
+
+type subItems []Item
+
+func (si subItems) String() string {
+	res := ""
+	for _, i := range si {
+		res += i.String()
+	}
+	return res
+}
+
+func (it Item) String() string {
+	switch it.typ {
+	case andOp:
+		return "&&"
+	case orOp:
+		return "||"
+	case leftBracket:
+		return "("
+	case rightBracket:
+		return ")"
+	case subExpression:
+		return "(" + it.sub.String() + ")"
+	case operand:
+		return it.op.String()
+	default:
+		return fmt.Sprintf("unknown item type: %v", it.typ)
+	}
+}
+
+func (it Item) isOperator() bool {
+	return it.typ == andOp || it.typ == orOp
+}
+
+func (it Item) isOperand() bool {
+	return it.typ == operand || it.typ == subExpression
+}
+
+// OR returns an OR item.
+func OR() Item {
+	return Item{typ: orOp}
+}
+
+// And returns an AND item.
+func AND() Item {
+	return Item{typ: andOp}
+}
+
+// LeftBracket returns a left bracket item.
+func LeftBracket() Item {
+	return Item{typ: leftBracket}
+}
+
+// RightBracket returns a right bracket item.
+func RightBracket() Item {
+	return Item{typ: rightBracket}
+}
+
+// T represents a boolean expression of regular expressions,
+// file type and mod time comparisons. It is evaluated against a single
+// input value.
+type T struct {
+	items []Item
+}
+
+// HasOperand returns true if the matcher's expression contains an instance
+// of the specified operand.
+func (m T) Needs(typ any) bool {
+	return needs(reflect.TypeOf(typ), m.items)
+}
+
+func needs(want reflect.Type, items []Item) bool {
+	for _, it := range items {
+		switch it.typ {
+		case operand:
+			if it.op.Needs(want) {
+				return true
+			}
+		case subExpression:
+			if needs(want, it.sub) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func newExpression(input <-chan Item) ([]Item, error) {
+	expr := []Item{}
+	for cur := range input {
+		switch cur.typ {
+		case operand:
+			op, err := cur.op.Prepare()
+			if err != nil {
+				return nil, err
+			}
+			expr = append(expr, NewOperandItem(op))
+		case andOp, orOp:
+			if len(expr) == 0 {
+				return nil, fmt.Errorf("missing left operand for %v", cur.typ)
+			}
+			if !expr[len(expr)-1].isOperand() {
+				return nil, fmt.Errorf("missing operand for %v", cur.typ)
+			}
+			expr = append(expr, cur)
+		case leftBracket:
+			if len(expr) > 0 && !expr[len(expr)-1].isOperator() {
+				return nil, fmt.Errorf("missing operator for %v", cur.typ)
+			}
+			sub, err := newExpression(input)
+			if err != nil {
+				return nil, err
+			}
+			expr = append(expr, Item{typ: subExpression, sub: sub})
+		case rightBracket:
+			if len(expr) == 0 {
+				return nil, fmt.Errorf("missing left operand for %v", cur.typ)
+			}
+			if !expr[len(expr)-1].isOperand() {
+				return nil, fmt.Errorf("missing operand for %v", cur.typ)
+			}
+			return expr, nil
+		}
+	}
+	return expr, nil
+}
+
+func itemChan(items []Item) <-chan Item {
+	itemCh := make(chan Item, len(items))
+	for _, it := range items {
+		itemCh <- it
+	}
+	close(itemCh)
+	return itemCh
+}
+
+// New returns a new matcher.T built from the supplied items.
+func New(items ...Item) (T, error) {
+	if len(items) == 0 {
+		return T{}, nil
+	}
+	// check for balanced ('s here since it's easy to do so.
+	balanced := 0
+	for _, it := range items {
+		if it.typ == leftBracket {
+			balanced++
+		}
+		if it.typ == rightBracket {
+			if balanced == 0 {
+				return T{}, fmt.Errorf("unbalanced brackets")
+			}
+			balanced--
+		}
+	}
+	if balanced != 0 {
+		return T{}, fmt.Errorf("unbalanced brackets")
+	}
+	tree, err := newExpression(itemChan(items))
+	if err != nil {
+		return T{}, err
+	}
+	if len(tree)%2 == 0 {
+		return T{}, fmt.Errorf("incomplete expression: %v", items)
+	}
+	return T{items: tree}, nil
+}
+
+func (m T) String() string {
+	var out strings.Builder
+	for _, it := range m.items {
+		if it.typ == subExpression {
+			se := T{items: it.sub}
+			out.WriteString("(" + se.String() + ") ")
+			continue
+		}
+		out.WriteString(it.String() + " ")
+	}
+	return strings.TrimSpace(out.String())
+}
+
+// Eval evaluates the matcher against the supplied value. An empty, default
+// matcher will always return false.
+func (m T) Eval(v any) bool {
+	if len(m.items) == 0 {
+		return false
+	}
+	return eval(itemChan(m.items), v)
+}
+
+func eval(exprs <-chan Item, v any) bool {
+	values := []bool{}
+	operators := []itemType{}
+	for cur := range exprs {
+		switch cur.typ {
+		case operand:
+			values = append(values, cur.op.Eval(v))
+		case orOp:
+			if values[len(values)-1] {
+				// early return on true || ....
+				return true
+			}
+			operators = append(operators, cur.typ)
+		case andOp:
+			operators = append(operators, cur.typ)
+		case subExpression:
+			values = append(values, eval(itemChan(cur.sub), v))
+		}
+		// left to right evaluation
+		if len(values) == 2 && len(operators) == 1 {
+			switch operators[0] {
+			case andOp:
+				values = []bool{values[0] && values[1]}
+			case orOp:
+				values = []bool{values[0] || values[1]}
+			}
+			operators = []itemType{}
+		}
+	}
+	return values[0]
+}
---
+++ cmdutil/boolexpr/expression_test.go
@@ -0,0 +1,297 @@
+// Copyright 2023 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package boolexpr_test
+
+import (
+	"reflect"
+	"regexp"
+	"strings"
+	"testing"
+
+	"cloudeng.io/cmdutil/boolexpr"
+)
+
+type regexOp struct {
+	val  string
+	name string // allow some tests to use a different name for this operand.
+	re   *regexp.Regexp
+}
+
+func (eq regexOp) String() string {
+	if eq.name == "" {
+		eq.name = "re"
+	}
+	return eq.name + "=" + eq.val
+}
+
+func (eq regexOp) Document() string {
+	if eq.name == "" {
+		eq.name = "re"
+	}
+	return eq.name + ": regular expression"
+}
+
+func (eq regexOp) Prepare() (boolexpr.Operand, error) {
+	var err error
+	eq.re, err = regexp.Compile(eq.val)
+	return eq, err
+}
+
+func (eq regexOp) Eval(v any) bool {
+	return eq.re.MatchString(v.(string))
+}
+
+func (eq regexOp) Needs(t reflect.Type) bool {
+	return t == reflect.TypeOf("")
+}
+
+func parse(input string) []boolexpr.Item {
+	input = strings.ReplaceAll(input, "(", " ( ")
+	input = strings.ReplaceAll(input, ")", " ) ")
+	input = strings.ReplaceAll(input, "||", " || ")
+	input = strings.ReplaceAll(input, "&&", " && ")
+	items := []boolexpr.Item{}
+	tokens := strings.Split(input, " ")
+	for i := 0; i < len(tokens); i++ {
+		if len(tokens[i]) == 0 {
+			continue
+		}
+		switch tokens[i] {
+		case "||":
+			items = append(items, boolexpr.OR())
+		case "&&":
+			items = append(items, boolexpr.AND())
+		case "(":
+			items = append(items, boolexpr.LeftBracket())
+		case ")":
+			items = append(items, boolexpr.RightBracket())
+		default:
+			items = append(items, boolexpr.NewOperandItem(&regexOp{val: tokens[i]}))
+		}
+	}
+	return items
+}
+
+func TestFormating(t *testing.T) {
+	for _, tc := range []struct {
+		in  string
+		out string
+	}{
+		{"", ""},
+		{"foo", "re=foo"},
+		{"foo || bar", "re=foo || re=bar"},
+		{"foo && bar", "re=foo && re=bar"},
+		{"foo && bar || baz", "re=foo && re=bar || re=baz"},
+		{"foo || bar && baz", "re=foo || re=bar && re=baz"},
+		{"foo && (bar||baz)", "re=foo && (re=bar || re=baz)"},
+		{"(bar || baz) && foo", "(re=bar || re=baz) && re=foo"},
+		{"( bar && baz )", "(re=bar && re=baz)"},
+		{"(bar && (baz || foo)) || else", "(re=bar && (re=baz || re=foo)) || re=else"},
+	} {
+		expr, err := boolexpr.New(parse(tc.in)...)
+		if err != nil {
+			t.Errorf("%v: failed to create expression: %v", tc.in, err)
+			continue
+		}
+		if got, want := expr.String(), tc.out; got != want {
+			t.Errorf("%v: got %v, want %v", tc.in, got, want)
+		}
+	}
+}
+
+func evalTestCase(t *testing.T, in []boolexpr.Item, val any, want bool) {
+	t.Helper()
+	expr, err := boolexpr.New(in...)
+	if err != nil {
+		t.Errorf("failed to create expression: %v", err)
+		return
+	}
+	r := expr.Eval(val)
+	if got := r; got != want {
+		t.Errorf("%v: %v: got %v, want %v", expr, val, got, want)
+	}
+}
+
+func TestOperators(t *testing.T) {
+	for _, tc := range []struct {
+		in  string
+		val any
+		out bool
+	}{
+		{"", "foo", false},
+		{`^fo && .ext$`, "foo.ext", true},
+		{`^fo && .ext$`, "foo", false},
+		{`^fo || .ext$`, "foo", true},
+		{`^fo || .ext$`, "x.ext", true},
+		{`^fo || .ext$`, "not", false},
+		{`^fo || .ext$ || wombat`, "foo.ext", true},
+		{`^fo || .ext$ || wombat`, "wombat", true},
+		{`^fo || .ext$ && wombat`, "wombat.ext", true},
+		{`^fo && .ext$ || wombat`, "foo.ext", true},
+		{`^fo && .ext$ || wombat`, "wombat", true},
+		{`^fo && (.ext$ || wombat)`, "wombat", false},
+		{`^fo && (.ext$ || wombat)`, "wombat.ext", false},
+		{`^fo && .ext$ && wombat`, "wombat", false},
+	} {
+		evalTestCase(t, parse(tc.in), tc.val, tc.out)
+	}
+}
+
+func TestSubExpressions(t *testing.T) {
+	for _, tc := range []struct {
+		in  string
+		val any
+		out bool
+	}{
+		{`(foo || bar)`, "foo", true},
+		{`(foo || bar)`, "wombat", false},
+		{`(foo || bar) || wom.*`, "wombat", true},
+		{`wo* || ( foo || bar )`, "wombat", true},
+		{`wo.* && (foo || bar)`, "wombat", false},
+		{`wo.* && (foo || .ext$)`, "wombat.ext", true},
+		{`wo.* && (foo || .ext$)`, "foo.ext", false},
+		{`(foo || \.ext$) && (wombat && \.ext$)`, "wombat.ext", true},
+		{`(foo || \.ext$) && (wombat && \.ext$)`,
+			"wombat", false},
+		{`(foo || \.ext$) || (wombat && \.ext$)`, "wombat", false},
+		{`(foo || \.ext$) || (wombat && \.ext$)`, "wombat", false},
+		{`(foo && (baz || bar))`, "foo", false},
+		{`(foo && (baz || bar))`, "baz", false},
+		{`(foo && (baz || bar))`, "foobar", true},
+		{`(^foo && (^baz || ^bar))`, "foobar", false},
+	} {
+		evalTestCase(t, parse(tc.in), tc.val, tc.out)
+	}
+}
+
+func TestErrors(t *testing.T) {
+	for _, tc := range []struct {
+		in  string
+		err string
+	}{
+		{`(`, "unbalanced brackets"},
+		{`()`, "missing left operand for )"},
+		{`(foo || bar`, "unbalanced brackets"},
+		{`foo || bar)`, "unbalanced brackets"},
+		{`)(`, "unbalanced brackets"},
+		{`||`, "missing left operand for ||"},
+		{`|| a`, "missing left operand for ||"},
+		{`a ||`, "incomplete expression: [re=a ||]"},
+		{`&&`, "missing left operand for &&"},
+		{`&& a`, "missing left operand for &&"},
+		{`a &&`, "incomplete expression: [re=a &&]"},
+		{`a || b || ()`, "missing left operand for )"},
+		{`( a || )`, "missing operand for )"},
+		{`( a () )`, "missing operator for ("},
+		{`|| ||`, "missing left operand for ||"},
+		{`a || ||`, "missing operand for ||"},
+		{`&& &&`, "missing left operand for &&"},
+		{`a && &&`, "missing operand for &&"},
+		{`a (a)`, "missing operator for ("},
+		{`[a-z+`, "error parsing regexp: missing closing ]: `[a-z+`"},
+	} {
+		m, err := boolexpr.New(parse(tc.in)...)
+		if err == nil || err.Error() != tc.err {
+			t.Errorf("%v: got %v, want %v", tc.in, err, tc.err)
+		}
+		if got, want := m.Eval("foo"), false; got != want {
+			t.Errorf("%v: got %v, want %v", tc.in, got, want)
+		}
+	}
+}
+
+type nameIfc interface {
+	Name() string
+}
+
+type regexNameOp struct{}
+
+func (rno regexNameOp) Needs(t reflect.Type) bool {
+	needs := reflect.TypeOf((*nameIfc)(nil)).Elem()
+	return t.Implements(needs)
+}
+
+func (rno regexNameOp) String() string {
+	return "nre="
+}
+
+func (rno regexNameOp) Document() string {
+	return "nre: regular expression"
+}
+
+func (rno regexNameOp) Prepare() (boolexpr.Operand, error) {
+	return rno, nil
+}
+
+func (rno regexNameOp) Eval(_ any) bool {
+	return false
+}
+
+type nifcImpl struct{}
+
+func (n nifcImpl) Name() string {
+	return "foo"
+}
+
+func TestNeeds(t *testing.T) {
+	var err error
+	var expr boolexpr.T
+	assert := func(strT, nameT, nameTPtr bool) {
+		t.Helper()
+		if err != nil {
+			t.Errorf("failed to create expression: %v", err)
+		}
+		if got, want := expr.Needs(""), strT; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+		if got, want := expr.Needs(nifcImpl{}), nameT; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+		if got, want := expr.Needs(&nifcImpl{}), nameTPtr; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+	expr, err = boolexpr.New()
+	assert(false, false, false)
+
+	expr, _ = boolexpr.New(boolexpr.NewOperandItem(regexOp{}))
+	assert(true, false, false)
+
+	expr, err = boolexpr.New(
+		boolexpr.NewOperandItem(&regexOp{}),
+		boolexpr.OR(),
+		boolexpr.NewOperandItem(&regexNameOp{}))
+
+	assert(true, true, true)
+
+	expr, err = boolexpr.New(boolexpr.NewOperandItem(&regexNameOp{}))
+	assert(false, true, true)
+
+	expr, err = boolexpr.New(boolexpr.LeftBracket(),
+		boolexpr.NewOperandItem(&regexOp{}),
+		boolexpr.RightBracket())
+	assert(true, false, false)
+
+	expr, err = boolexpr.New(
+		boolexpr.LeftBracket(),
+		boolexpr.NewOperandItem(&regexOp{}),
+		boolexpr.OR(),
+		boolexpr.LeftBracket(),
+		boolexpr.NewOperandItem(&regexOp{}),
+		boolexpr.RightBracket(),
+		boolexpr.RightBracket())
+	assert(true, false, false)
+
+	expr, err = boolexpr.New(
+		boolexpr.LeftBracket(),
+		boolexpr.NewOperandItem(&regexOp{}),
+		boolexpr.OR(),
+		boolexpr.LeftBracket(),
+		boolexpr.NewOperandItem(&regexNameOp{}),
+		boolexpr.RightBracket(),
+		boolexpr.RightBracket())
+	assert(true, true, true)
+
+}
