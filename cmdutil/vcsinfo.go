ca7e90b17f676eb1da98a8ea49acdb15c03abada
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-12-15 09:26:32 -0800

    .

--- cmdutil/vcsinfo.go
+++ cmdutil/vcsinfo.go
@@ -15,14 +15,18 @@ func VCSInfo() (revision string, lastCommit time.Time, dirty, ok bool) {
 	if !ok {
 		return
 	}
+	ok = false
 	for _, kv := range info.Settings {
 		switch kv.Key {
 		case "vcs.revision":
 			revision = kv.Value
+			ok = true
 		case "vcs.time":
 			lastCommit, _ = time.Parse(time.RFC3339, kv.Value)
+			ok = true
 		case "vcs.modified":
 			dirty = kv.Value == "true"
+			ok = true
 		}
 	}
 	return
