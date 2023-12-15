da28b512157afcc287f88ea1d5366f7d48f5fa73
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-12-15 09:20:26 -0800

    file/filewalk: define XAttr to represent uid, gid etc to be managed in an filesystem dependent manner. (#238)

---
+++ file/filewalk/localfs/sys_linux_amd64.go
@@ -0,0 +1,43 @@
+// Copyright 2023 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+// go:build linux && amd64
+package localfs
+
+import (
+	"fmt"
+	"syscall"
+
+	"cloudeng.io/file"
+	"cloudeng.io/file/filewalk"
+)
+
+func xAttr(pathname string, fi file.Info) (filewalk.XAttr, error) {
+	si := fi.Sys()
+	if si == nil {
+		return filewalk.XAttr{}, fmt.Errorf("no system set for %v", pathname)
+	}
+	if s, ok := si.(*syscall.Stat_t); ok {
+		return filewalk.XAttr{
+			UID:       uint64(s.Uid),
+			GID:       uint64(s.Gid),
+			Device:    s.Dev,
+			FileID:    s.Ino,
+			Blocks:    s.Blocks,
+			Hardlinks: s.Nlink,
+		}, nil
+	}
+	return filewalk.XAttr{}, fmt.Errorf("unrecognised system information %T for %v", si, pathname)
+}
+
+func newXAttr(xattr filewalk.XAttr) any {
+	return &syscall.Stat_t{
+		Uid:    uint32(xattr.UID & 0xffffffff),
+		Gid:    uint32(xattr.GID & 0xffffffff),
+		Dev:    xattr.Device,
+		Ino:    xattr.FileID,
+		Blocks: xattr.Blocks,
+		Nlink:  xattr.Hardlinks,
+	}
+}
---
+++ file/filewalk/localfs/sys_windows.go
@@ -0,0 +1,87 @@
+// Copyright 2023 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+// go:build windows
+package localfs
+
+import (
+	"fmt"
+	"syscall"
+
+	"cloudeng.io/file"
+	"cloudeng.io/file/filewalk"
+	"golang.org/x/sys/windows"
+)
+
+type sysinfo struct {
+	uid, gid       uint64
+	device, fileID uint64
+	blocks         int64
+	hardlinks      uint64
+}
+
+func xAttr(pathname string, fi file.Info) (filewalk.XAttr, error) {
+	si := fi.Sys()
+	if si == nil {
+		return getSysInfo(pathname)
+	}
+	switch s := si.(type) {
+	case *sysinfo:
+		return filewalk.XAttr{
+			UID:       s.uid,
+			GID:       s.gid,
+			Device:    s.device,
+			FileID:    s.fileID,
+			Blocks:    s.blocks,
+			Hardlinks: s.hardlinks,
+		}, nil
+	}
+	return getSysInfo(pathname)
+}
+
+func newXAttr(xattr filewalk.XAttr) any {
+	return &sysinfo{
+		uid:       xattr.UID,
+		gid:       xattr.GID,
+		device:    xattr.Device,
+		fileID:    xattr.FileID,
+		blocks:    xattr.Blocks,
+		hardlinks: xattr.Hardlinks,
+	}
+}
+
+func packFileIndices(hi, low uint32) uint64 {
+	return uint64(hi)<<32 | uint64(low)
+}
+
+func getSysInfo(pathname string) (filewalk.XAttr, error) {
+	// taken from loadFileId in types_windows.go
+	pathp, err := syscall.UTF16PtrFromString(pathname)
+	if err != nil {
+		return filewalk.XAttr{}, fmt.Errorf("failed to convert %v to win32 utf16p: %v", pathname, err)
+	}
+	attrs := uint32(syscall.FILE_FLAG_BACKUP_SEMANTICS | syscall.FILE_FLAG_OPEN_REPARSE_POINT)
+	h, err := windows.CreateFile(pathp, 0, 0, nil, syscall.OPEN_EXISTING, attrs, 0)
+	if err != nil {
+		return filewalk.XAttr{}, fmt.Errorf("CreateFile OPEN_EXISTING: failed to open %v: %v", pathname, err)
+	}
+	defer windows.CloseHandle(h)
+	var d windows.ByHandleFileInformation
+	if err = windows.GetFileInformationByHandle(h, &d); err != nil {
+		return filewalk.XAttr{}, fmt.Errorf("GetFileInformationByHandle for %v: %v", pathname, err)
+	}
+	size := int64(uint64(d.FileSizeHigh)<<32 | uint64(d.FileSizeLow))
+	blocks := size / 512
+	if blocks == 0 {
+		blocks = 1
+	}
+	return filewalk.XAttr{
+		UID:       0,
+		GID:       0,
+		Device:    uint64(d.VolumeSerialNumber),
+		FileID:    packFileIndices(d.FileIndexHigh, d.FileIndexLow),
+		Blocks:    blocks,
+		Hardlinks: uint64(d.NumberOfLinks),
+	}, nil
+}
