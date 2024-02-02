309723de87d275fb4d83a01d85c4701a1b770675
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2024-02-02 09:25:33 -0800

    .

--- path/cloudpath/cloud_join.go
+++ path/cloudpath/cloud_join.go
@@ -46,28 +46,17 @@ func Join(sep byte, components []string) string {
 	return string(joined)
 }
 
-func lastIdx(scheme string, sep byte, path string) int {
-	idx := strings.LastIndexByte(path, sep)
-	if idx < 0 {
-		return -1
-	}
-	if idx <= len(scheme) && strings.HasPrefix(path, scheme) {
-		return -2
-	}
-	return idx
-}
-
 // Base is like path.Base but for cloud storage paths which may include
 // a scheme (eg. s3://). It does not support URI host names, parameters etc.
 // In particular:
 //   - the scheme parameter should include the trailing :// or be the
 //     empty string.
-//   - a trailing seperator means that the path is a prefix with
+//   - a trailing separator means that the path is a prefix with
 //     an empty base and hence Base returns "".
 //   - the returned basename never includes the supplied scheme.
-func Base(scheme string, seperator byte, path string) string {
+func Base(scheme string, separator byte, path string) string {
 	p := strings.TrimPrefix(path, scheme)
-	idx := strings.LastIndexByte(p, seperator)
+	idx := strings.LastIndexByte(p, separator)
 	if idx < 0 {
 		return path
 	}
@@ -80,14 +69,14 @@ func Base(scheme string, seperator byte, path string) string {
 //   - the scheme parameter should include the trailing :// or be the
 //     empty string.
 //   - the returned prefix never includes the supplied scheme.
-//   - the returned prefix never includes a trailing seperator.
-func Prefix(scheme string, seperator byte, path string) string {
+//   - the returned prefix never includes a trailing separator.
+func Prefix(scheme string, separator byte, path string) string {
 	p := strings.TrimPrefix(path, scheme)
-	if len(p) > 0 && p[len(p)-1] == seperator {
+	if len(p) > 0 && p[len(p)-1] == separator {
 		p = p[:len(p)-1] // already a prefix
 		return p
 	}
-	if idx := strings.LastIndexByte(p, seperator); idx >= 0 {
+	if idx := strings.LastIndexByte(p, separator); idx >= 0 {
 		return p[:idx]
 	}
 	return ""
