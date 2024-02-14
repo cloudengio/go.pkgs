ef7cb11b3290a5bb47fbcb45b872af5a13828e7d
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2024-02-14 09:02:42 -0800

    .

--- aws/awstestutil/skip_darwin.go
+++ aws/awstestutil/skip_darwin.go
@@ -7,11 +7,14 @@
 package awstestutil
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
 func isOnGitHubActions() bool {
+	fmt.Printf("GITHUB_ACTIONS: %v\n", os.Getenv("GITHUB_ACTIONS"))
+	fmt.Printf("ENV: %v\n", os.Environ())
 	return os.Getenv("GITHUB_ACTIONS") != ""
 }
 
