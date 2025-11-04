ed70f2422b72f61ab08ec43eabb3e524d997804f
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2025-11-03 21:57:48 -0800

    .

--- webapp/devtest/ts_test.go
+++ webapp/devtest/ts_test.go
@@ -100,6 +100,9 @@ func readFile(t *testing.T, fn string) string {
 }
 
 func TestTypescriptSources_CompileIncremental(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("skipping for windows - needs investigation")
+	}
 	tmp := t.TempDir()
 	copyTestdata(t, tmp, "one.ts", "two.ts")
 
