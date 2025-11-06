f5bffac3ff3dd77d49c7a8b7c6dd54aef4912b9c
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2025-11-06 11:27:11 -0800

    .

--- webapp/devtest/chromedputil/util.go
+++ webapp/devtest/chromedputil/util.go
@@ -315,7 +315,7 @@ var (
 		chromedp.Flag("disable-breakpad", true),
 		chromedp.Flag("disable-crash-reporter", true),
 		chromedp.Flag("disable-component-update", true),
-		chromedp.Flag("disable-features", "MetricsReporting,UserMetrics"),
+		chromedp.Flag("disable-features", "NetworkService,MetricsReporting,UserMetrics"),
 	}
 
 	AllocatorOptsVerboseLogging = []chromedp.ExecAllocatorOption{
