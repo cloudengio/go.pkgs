e134ff18a87571a9d0c229f65c35d5892822f339
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2025-11-19 09:47:46 -0800

    macos: delete this module

--- macos/keychain/keychaincmd/cmdusage.go
+++
@@ -1,7 +0,0 @@
-// Usage of /var/folders/dy/3b1mfs651m1cdwzr6xd9j4dm0000gn/T/go-build1915994116/b001/exe/keychaincmd:
-//
-//	-account string
-//	  	keychain account that the note belongs to (default "cnicolaou")
-//	-service string
-//	  	keychain service that the note belongs to
-package main
