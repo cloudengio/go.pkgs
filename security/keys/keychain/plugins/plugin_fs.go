3ffb13f1a1c78f33fe52ed1383f527733a458e14
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2025-11-20 10:18:53 -0800

    .

--- security/keys/keychain/plugins/plugin_fs.go
+++ security/keys/keychain/plugins/plugin_fs.go
@@ -12,18 +12,25 @@ import (
 	"os"
 )
 
+// FS implements a plugin-based file system for keychain
+// that implements file.ReadFileFS and file.WriteFileFS.
 type FS struct {
 	sysSpecific any
 	binary      string
 	args        []string
 }
 
+// NewFS creates a new FS instance with the specified plugin path, system-specific data, and plugin arguments.
 func NewFS(pluginPath string, sysSpecific any, args ...string) (*FS, error) {
 	return &FS{
 		sysSpecific: sysSpecific,
 		binary:      pluginPath,
 		args:        args,
 	}, nil
+}
+
+func (f *FS) ReadFile(name string) ([]byte, error) {
+	return f.ReadFileCtx(context.Background(), name)
 }
 
 func (f *FS) ReadFileCtx(ctx context.Context, name string) ([]byte, error) {
@@ -42,6 +49,10 @@ func (f *FS) ReadFileCtx(ctx context.Context, name string) ([]byte, error) {
 		return nil, fmt.Errorf("plugin error: %s", resp.Error)
 	}
 	return base64.StdEncoding.DecodeString(resp.Contents)
+}
+
+func (fs *FS) WriteFile(name string, data []byte) error {
+	return fs.WriteFileCtx(context.Background(), name, data)
 }
 
 func (f *FS) WriteFileCtx(ctx context.Context, name string, data []byte) error {
