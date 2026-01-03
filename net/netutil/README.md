a46dd087d2865e3d01a4084cdb432cecd6157c44
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2026-01-03 12:28:10 -0800

    net/netutil: add address parsing routines, update README.md (#467)

---
+++ net/netutil/README.md
@@ -0,0 +1,48 @@
+# Package [cloudeng.io/net/netutil](https://pkg.go.dev/cloudeng.io/net/netutil?tab=doc)
+
+```go
+import cloudeng.io/net/netutil
+```
+
+Package netutil provides utility functions for networking, including parsing
+IP addresses and prefixes.
+
+## Functions
+### Func HTTPServerAddr
+```go
+func HTTPServerAddr(addrPort netip.AddrPort) string
+```
+HTTPServerAddr returns the address of an HTTP server based on the address
+and port of the server in a form to be used with http.Server.Addr.
+If the address is unspecified then it will be replaced with an empty string.
+If the port is 80 then "http" will be appended to the address, if the port
+is 443 then "https" will be appended to the address, otherwise the numeric
+port will be used.
+
+### Func ParseAddrDefaultPort
+```go
+func ParseAddrDefaultPort(addr, port string) (netip.AddrPort, error)
+```
+ParseAddrDefaultPort parses an IP address string. If the address string
+already contains a port, it is parsed and returned. Otherwise, the supplied
+default port is used to construct and parse an address with that port.
+
+### Func ParseAddrIgnoringPort
+```go
+func ParseAddrIgnoringPort(addr string) (netip.Addr, error)
+```
+ParseAddrIgnoringPort parses an IP address string and returns the address.
+If the string is an address with a port, it will be parsed as an address
+with a port and the address will be returned, ignoring the port.
+
+### Func ParseAddrOrPrefix
+```go
+func ParseAddrOrPrefix(addr string) (netip.Addr, error)
+```
+ParseAddrOrPrefix parses an IP address or prefix string and returns the
+address. If the string is an IP address without a prefix, it is treated as a
+full-bit prefix (/32 for IPv4, /128 for IPv6).
+
+
+
+
---
+++ net/netutil/parse.go
@@ -0,0 +1,83 @@
+// Copyright 2025 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+// Package netutil provides utility functions for networking, including
+// parsing IP addresses and prefixes.
+package netutil
+
+import (
+	"net"
+	"net/netip"
+	"strconv"
+	"strings"
+)
+
+// ParseAddrOrPrefix parses an IP address or prefix string and returns the
+// address. If the string is an IP address without a prefix, it is treated
+// as a full-bit prefix (/32 for IPv4, /128 for IPv6).
+func ParseAddrOrPrefix(addr string) (netip.Addr, error) {
+	if strings.Contains(addr, "/") {
+		p, err := netip.ParsePrefix(addr)
+		if err != nil {
+			return netip.Addr{}, err
+		}
+		return p.Addr(), nil
+	}
+	return netip.ParseAddr(addr)
+}
+
+// ParseAddrIgnoringPort parses an IP address string and returns the address.
+// If the string is an address with a port, it will be parsed as an address
+// with a port and the address will be returned, ignoring the port.
+func ParseAddrIgnoringPort(addr string) (netip.Addr, error) {
+	ap, err := netip.ParseAddrPort(addr)
+	if err == nil {
+		return ap.Addr(), nil
+	}
+	return netip.ParseAddr(addr)
+}
+
+// ParseAddrDefaultPort parses an IP address string. If the address string
+// already contains a port, it is parsed and returned. Otherwise, the
+// supplied default port is used to construct and parse an address with
+// that port.
+func ParseAddrDefaultPort(addr, port string) (netip.AddrPort, error) {
+	ap, err := netip.ParseAddrPort(addr)
+	if err == nil {
+		return ap, nil
+	}
+	if len(addr) == 0 {
+		addr = "0.0.0.0"
+	}
+	switch port {
+	case "http":
+		port = "80"
+	case "https":
+		port = "443"
+	}
+	return netip.ParseAddrPort(net.JoinHostPort(addr, port))
+}
+
+// HTTPServerAddr returns the address of an HTTP server based on the
+// address and port of the server in a form to be used with http.Server.Addr.
+// If the address is unspecified then it will be replaced with an empty string.
+// If the port is 80 then "http" will be appended to the address, if the
+// port is 443 then "https" will be appended to the address, otherwise the
+// numeric port will be used.
+func HTTPServerAddr(addrPort netip.AddrPort) string {
+	var addr string
+	if addrPort.Addr().IsUnspecified() {
+		addr = ""
+	} else {
+		addr = addrPort.Addr().String()
+	}
+	switch addrPort.Port() {
+	case 80:
+		return net.JoinHostPort(addr, "http")
+	case 443:
+		return net.JoinHostPort(addr, "https")
+	default:
+		return net.JoinHostPort(addr, strconv.Itoa(int(addrPort.Port())))
+	}
+}
