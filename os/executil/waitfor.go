830ef619d82732c677f326b0424e18b75182ab58
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2026-04-23 17:29:40 -0700

    os/executil: add WaitFor function (#516)

---
+++ os/executil/waitfor.go
@@ -0,0 +1,40 @@
+// Copyright 2026 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package executil
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+// WaitFor repeatedly calls the provided check function until it returns
+// done=true, or the context is done. It waits for the specified
+// interval between calls. If check returns an error, it is ignored
+// unless done=true, in which case it is returned immediately.
+func WaitFor(ctx context.Context, interval time.Duration, check func(ctx context.Context) (done bool, err error)) error {
+	if interval <= 0 {
+		return fmt.Errorf("executil: WaitFor: interval must be positive: %v", interval)
+	}
+
+	if done, err := check(ctx); done {
+		return err
+	}
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			if done, err := check(ctx); done {
+				return err
+			}
+		}
+	}
+
+}
