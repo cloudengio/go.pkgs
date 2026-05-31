eefa29879409ee30398a0840a99da3bf9dd27788
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2026-05-31 09:30:03 -0700

    .

--- aws/cmd/aws-secrets/cmdusage.go
+++
@@ -1,7 +0,0 @@
-// Usage of aws-secrets
-//
-//	provide access to aws secretsmanager across multiple operating systems
-//
-//	 read - read a secret from aws secretsmanager
-//	write - write a secret to aws secretsmanager
-package main
