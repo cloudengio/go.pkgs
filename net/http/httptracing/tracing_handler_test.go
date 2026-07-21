610e2eef53c2fececa214a327896791c55f40639
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2026-07-21 15:21:43 -0700

    net/.../httptracing: prevent stack trace from being written to client

--- net/http/httptracing/tracing_handler_test.go
+++ net/http/httptracing/tracing_handler_test.go
@@ -167,7 +167,7 @@ func TestTracingHandlerPanicRecovery(t *testing.T) {
 		logger := slog.New(slog.NewJSONHandler(&logBuf, nil))
 
 		handler := NewTracingHandler(
-			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
 				panic("something went wrong")
 			}),
 			WithTraceHandlerLogger(logger),
@@ -209,7 +209,7 @@ func TestTracingHandlerPanicRecovery(t *testing.T) {
 		logger := slog.New(slog.NewJSONHandler(&logBuf, nil))
 
 		handler := NewTracingHandler(
-			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
 				_, _ = w.Write([]byte("partial"))
 				panic("panic after implicit write")
 			}),
