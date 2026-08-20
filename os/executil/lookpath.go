7ea82e433de72908c9b94969e5d5fd75273c84fe
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2026-08-20 07:59:05 -0700

    os/executil: add AppendMissingPathComponents and AcquireRunLock

--- os/executil/lookpath.go
+++ os/executil/lookpath.go
@@ -4,7 +4,11 @@
 
 package executil
 
-import "strings"
+import (
+	"os"
+	"slices"
+	"strings"
+)
 
 // ReplaceEnvVar replaces the value of an environment variable in the provided slice.
 // If the variable does not exist, it is added to the slice.
@@ -27,4 +31,18 @@ func Getenv(env []string, key string) (string, bool) {
 		}
 	}
 	return "", false
+}
+
+// AppendMissingPathComponents treats pathList as a path list, ie. a list of paths
+// separated by os.PathListSeparator. It returns a copy of pathList with any of
+// the specified paths that do not already exist in pathList appended to the
+// end of the returned string.
+func AppendMissingPathComponents(pathList string, paths ...string) string {
+	pathComponents := strings.Split(pathList, string(os.PathListSeparator))
+	for _, c := range paths {
+		if !slices.Contains(pathComponents, c) {
+			pathComponents = append(pathComponents, c)
+		}
+	}
+	return strings.Join(pathComponents, string(os.PathListSeparator))
 }
