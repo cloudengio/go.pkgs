e2462c86ca71e9d1d0e5dda7446d2794a5b0b492
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2026-09-09 09:10:24 -0700

    cmdutil/executil: minor changes to command lines for go build etc

--- os/executil/exec.go
+++ os/executil/exec.go
@@ -10,12 +10,41 @@ import (
 	"os/exec"
 )
 
+// GoBuild builds the specified Go binary using the provided context and
+// arguments. It returns the path to the built binary or an error if the build
+// fails.
 func GoBuild(ctx context.Context, binary string, args ...string) (string, error) {
-	binary = ExecName(binary)
-	cmd := exec.CommandContext(ctx, "go", append([]string{"build", "-o", binary}, args...)...) // #nosec G204
+	gobin, goargs, err := GoBuildArgs(binary, args...)
+	if err != nil {
+		return "", err
+	}
+	cmd := exec.CommandContext(ctx, gobin, goargs...)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", fmt.Errorf("%s: %v", out, err)
 	}
 	return binary, nil
+}
+
+// GoBuildArgs returns the Go executable path, the arguments for a Go build
+// command that outputs the specified binary, or an error if the Go executable
+// could not be found.
+// GoBuildArgs("mybinary", "a", "b") might return "/usr/local/go/bin/go", ["build", "-o", "mybinary", "a", "b"], nil.
+func GoBuildArgs(binary string, args ...string) (string, []string, error) {
+	gobin, err := exec.LookPath("go")
+	if err != nil {
+		return "", nil, fmt.Errorf("could not find 'go' executable: %v", err)
+	}
+	return gobin, append([]string{"build", "-o", ExecName(binary)}, args...), nil
+}
+
+// GoInstallArgs returns the Go executable path and the arguments fo
+// a Go install command, or an error if the Go executable could not be found.
+// GoInstall("a", "b") might return "/usr/local/go/bin/go", ["install", "a", "b"], nil.
+func GoInstallArgs(args ...string) (string, []string, error) {
+	gobin, err := exec.LookPath("go")
+	if err != nil {
+		return "", nil, fmt.Errorf("could not find 'go' executable: %v", err)
+	}
+	return gobin, append([]string{"install"}, args...), nil
 }
